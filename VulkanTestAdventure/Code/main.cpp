8d29c0a40128fe130adcef08b1fa90d4ecac7de1
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-08-26 23:29:02 +0300

    Swapchain created

--- VulkanTestAdventure/Code/main.cpp
+++ VulkanTestAdventure/Code/main.cpp
@@ -1,35 +1,180 @@
+#include <vulkan/vulkan_raii.hpp>
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.h>
 
-int main(void)
-{
-    GLFWwindow* window;
+#include <iostream>
+#include <unordered_set>
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to init GLFW" << std::endl;
         return -1;
     }
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "VulkanTestAdventure", nullptr, nullptr);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    uint32_t glfw_extensions_count = 0;
+    const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extensions_count);
+    std::vector<const char*> extensions(glfw_extensions, glfw_extensions + glfw_extensions_count);
 
-    /* Loop until the user closes the window */
+    VkApplicationInfo app_info{};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = "VulkanTestAdventure";
+    app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.pEngineName = nullptr;
+    app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.apiVersion = VK_API_VERSION_1_4;
+
+    VkInstanceCreateInfo create_info{};
+    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    create_info.pApplicationInfo = &app_info;
+    create_info.enabledExtensionCount = extensions.size();
+    create_info.ppEnabledExtensionNames = extensions.data();
+
+    VkInstance instance{};
+    if (vkCreateInstance(&create_info, nullptr, &instance) != VK_SUCCESS)
+        throw std::runtime_error("ERROR : Failed to create Vulkan Instance");
+
+    VkSurfaceKHR surface{};
+    glfwCreateWindowSurface(instance, window, nullptr, &surface);
+
+    uint32_t device_count = 0;
+    vkEnumeratePhysicalDevices(instance, &device_count, nullptr);
+    if (device_count == 0) throw std::runtime_error("ERRRO : No Vulkan devices found");
+    std::vector<VkPhysicalDevice> devices(device_count);
+    vkEnumeratePhysicalDevices(instance, &device_count, devices.data());
+
+    VkPhysicalDevice physical_device = devices[0];
+
+    uint32_t queue_family_count = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queue_family_count, queue_families.data());
+
+    uint32_t graphics_queue_family_index = 0;
+    uint32_t present_queue_family_index = 0;
+
+    for (size_t i = 0; i < queue_families.size(); i++) {
+        VkQueueFamilyProperties queue_family = queue_families[i];
+
+        VkBool32 present_support = false;
+        vkGetPhysicalDeviceSurfaceSupportKHR(physical_device, i, surface, &present_support);
+
+        if (present_support) present_queue_family_index = i;
+
+        if (queue_family.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            graphics_queue_family_index = i;
+            break;
+        }
+    }
+
+    if (graphics_queue_family_index == UINT32_MAX)
+        throw std::runtime_error("ERROR : No graphics queue found");
+
+    VkPhysicalDeviceFeatures device_features{};
+
+    float queue_priority = 1.0f;
+
+    std::vector<VkDeviceQueueCreateInfo> queue_create_infos;
+    std::unordered_set<uint32_t> unique_queue_families = {
+        graphics_queue_family_index,
+        present_queue_family_index
+    };
+
+    for (uint32_t queue_family : unique_queue_families) {
+        VkDeviceQueueCreateInfo queue_create_info{};
+        queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queue_create_info.queueCount = 1;
+        queue_create_info.queueFamilyIndex = graphics_queue_family_index;
+        queue_create_info.pQueuePriorities = &queue_priority;
+
+        queue_create_infos.emplace_back(queue_create_info);
+    }
+
+    VkDeviceCreateInfo device_create_info{};
+    device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+    const std::vector<const char*> device_extensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
+    device_create_info.enabledExtensionCount = device_extensions.size();
+    device_create_info.ppEnabledExtensionNames = device_extensions.data();
+    device_create_info.pEnabledFeatures = &device_features;
+
+    device_create_info.queueCreateInfoCount = queue_create_infos.size();
+    device_create_info.pQueueCreateInfos = queue_create_infos.data();
+
+    VkDevice device{};
+    if (vkCreateDevice(physical_device, &device_create_info, nullptr, &device) != VK_SUCCESS)
+        throw std::runtime_error("ERROR : Failed to create device");
+
+    uint32_t format_count = 0;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physical_device, surface, &format_count, nullptr);
+    std::vector<VkSurfaceFormatKHR> swapchain_formats(format_count);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physical_device, surface, &format_count, swapchain_formats.data());
+
+    VkSurfaceFormatKHR surface_format = swapchain_formats[0];
+    for (VkSurfaceFormatKHR avaliable_format : swapchain_formats) {
+        if (avaliable_format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR && avaliable_format.format == VK_FORMAT_B8G8R8A8_SRGB) {
+            surface_format = avaliable_format;
+            break;
+        }
+    }
+
+    VkSurfaceCapabilitiesKHR capabilities{};
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physical_device, surface, &capabilities);
+    VkExtent2D extent = capabilities.currentExtent;
+    uint32_t image_count = capabilities.minImageCount + 1;
+
+    VkSwapchainCreateInfoKHR swapchain_create_info{};
+    swapchain_create_info.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    swapchain_create_info.surface = surface;
+    swapchain_create_info.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    swapchain_create_info.clipped = VK_TRUE;
+    swapchain_create_info.oldSwapchain = VK_NULL_HANDLE;
+    swapchain_create_info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapchain_create_info.imageArrayLayers = 1;
+    
+    uint32_t queue_family_indices[] = { graphics_queue_family_index, present_queue_family_index };
+    if (graphics_queue_family_index != present_queue_family_index) {
+        swapchain_create_info.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+        swapchain_create_info.queueFamilyIndexCount = 2;
+        swapchain_create_info.pQueueFamilyIndices = queue_family_indices;
+    }
+    else swapchain_create_info.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    uint32_t present_mode_count = 0;
+    vkGetPhysicalDeviceSurfacePresentModesKHR(physical_device, surface, &present_mode_count, nullptr);
+    std::vector<VkPresentModeKHR> present_modes(present_mode_count);
+    vkGetPhysicalDeviceSurfacePresentModesKHR(physical_device, surface, &present_mode_count, present_modes.data());
+    
+    VkPresentModeKHR preset_mode = VK_PRESENT_MODE_MAILBOX_KHR;
+    for (VkPresentModeKHR avaliable_mode : present_modes) {
+        if (avaliable_mode == VK_PRESENT_MODE_MAILBOX_KHR) {
+            preset_mode = avaliable_mode;
+            break;
+        }
+    }
+    swapchain_create_info.presentMode = preset_mode;
+    swapchain_create_info.imageExtent = extent;
+    swapchain_create_info.minImageCount = image_count;
+    swapchain_create_info.preTransform = capabilities.currentTransform;
+    swapchain_create_info.imageFormat = surface_format.format;
+    swapchain_create_info.imageColorSpace = surface_format.colorSpace;
+
+    VkSwapchainKHR swapchain{};
+    vkCreateSwapchainKHR(device, &swapchain_create_info, nullptr, &swapchain);
+
     while (!glfwWindowShouldClose(window)) {
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
+    vkDestroyDevice(device, nullptr);
+    vkDestroyInstance(instance, nullptr);
+
+    glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
