7ca3bfa2159a625028e294c11b67fb57637a0dbe
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-09-23 16:40:13 +0300

    Fixed 3D view. Multiple object renderer start

--- VulkanTestAdventure/Code/main.cpp
+++ VulkanTestAdventure/Code/main.cpp
@@ -1,5 +1,5 @@
 #define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#include <glfw3.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -9,10 +9,10 @@
 #include <glm/gtx/hash.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <STB/stb_image.h>
+#include <stb_image.h>
 
 #define TINYOBJLOADER_IMPLEMENTATION
-#include <TinyObjLoader/tiny_obj_loader.h>
+#include <tiny_obj_loader.h>
 
 #include <iostream>
 #include <fstream>
@@ -647,8 +647,8 @@ private:
     }
 
     void createGraphicsPipeline() {
-        auto vertShaderCode = readFile("C:/Users/Пользователь/Desktop/VulkanTestAdventure/Files/Shaders/SimpleTriangle/shader.vert.spv");
-        auto fragShaderCode = readFile("C:/Users/Пользователь/Desktop/VulkanTestAdventure/Files/Shaders/SimpleTriangle/shader.frag.spv");
+        auto vertShaderCode = readFile("C:/Users/Пользователь/Desktop/VulkanTestAdventure/Files/Shaders/Test1/shader.vert.spv");
+        auto fragShaderCode = readFile("C:/Users/Пользователь/Desktop/VulkanTestAdventure/Files/Shaders/Test1/shader.frag.spv");
 
         VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
         VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
@@ -1482,7 +1482,7 @@ private:
         UniformBufferObject ubo{};
         ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
+        ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 100.0f);
         ubo.proj[1][1] *= -1;
 
         memcpy(uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
